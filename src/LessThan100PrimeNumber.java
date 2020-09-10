5816696f4993bcc7e2c4f121a29847b48cdc1ed1
Thuvh94 <aloha.hat.2508@gmail.com>
2020-09-10 11:26:15 +0700

    Exercise: Print prime numbers that smaller than 100

---
+++ src/LessThan100PrimeNumber.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class LessThan100PrimeNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Insert limit");
+        int limit = scanner.nextInt();
+        int number = 2;
+        while (number <= limit) {
+            if (isPrime(number)) {
+                System.out.print(number+" ");
+            }
+            number++;
+        }
+    }
+
+    static boolean isPrime(int number) {
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0)
+                return false;
+        }
+        return true;
+    }
+}
+
+
+
